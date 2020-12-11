e0a0859c0516dacfb84cf7d6682ccdec29df7ff8
Giuliano Lopes Vieira Amorim <giuliano.amorim@al.infnet.edu.br>
2020-12-11 16:53:28 -0300

    first commit

---
+++ src/components/Config.js
@@ -0,0 +1,7 @@
+//SERVER ROUTES
+export const USER_SERVER = '/api/users';
+
+export const API_KEY = '057cb3f5e27e45a637de10a1cefde121'
+export const API_URL = 'https://api.themoviedb.org/3/'
+
+export const IMAGE_URL = 'https://image.tmdb/t/p/'
